a3a2b7031271cfa2f0bd5dc8996706646f128d44
Aurélien Aubert <aurelien.aubert@outlook.com>
2015-12-16 12:33:30 +0100

    "Fin"
    "Fin"

--- src/model.php
+++ src/model.php
@@ -1,7 +1,7 @@
 <?php
 
 // Return all news
-function getNews() {
+function getMovies() {
     $bdd = new PDO('mysql:host=localhost;dbname=mymovies;charset=utf8', 'mymovies_user', 'secret');
     $movies = $bdd->query('select * from movie order by mov_id desc');
     return $movies;
